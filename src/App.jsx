e2a26db87375fa435b5f9ff716504e8d21bdc422
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-08-28 19:27:08 +0600

    add pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+
 import Navbar from './components/Navbar'
+import Routes, { Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import Login from './pages/Login'
+import About from './pages/About'
+import Product from './pages/Product'
+import ProductCart from './pages/ProductCart'
+import PlaceOrder from './pages/PlaceOrder'
+import OrderList from './pages/OrderList'
+
 
 const App = () => {
   return (
-    <div>
+    
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+
       <Navbar/>
+
+    
+    <Routes>
       
+    <Route path={'/'} element={<Home/>}/>
+    <Route path={'/collection'} element={<Collection/>}/>
+    <Route path={'/login'} element={<Login/>}/>
+    <Route path={'/about'} element={<About/>}/>
+    <Route path={'/product/:productId'} element={<Product/>}/>
+    <Route path={'/productCart'} element={<ProductCart/>}/>
+    <Route path={'/place-order'} element={<PlaceOrder/>}/>
+    <Route path={'/orderList'} element={<OrderList/>}/>
+  
+
+
+    </Routes>
     </div>
   )
 }
