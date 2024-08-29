261a6be88cd04e8fc65ea26dede5bf7f48068b7d
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-08-29 19:22:02 +0600

    add shop context

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import { assets } from "../assets/assets"
 
 const HeroSection = () => {
   return (
-    <div className="flex flex-col sm:flex-row border border-gray-700">
+    <div className="flex flex-col sm:flex-row border border-gray-700 rounded">
         {/* Hero Left */}
         <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
         <div className="text-[#414141]">
