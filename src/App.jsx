1bcbf2ab763f60d85694c7390edbc276178ec818
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-08-29 10:46:54 +0600

    add navbar part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 
 import Navbar from './components/Navbar'
-import React from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import Collection from './pages/Collection'
